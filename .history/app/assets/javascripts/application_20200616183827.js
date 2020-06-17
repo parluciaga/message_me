d824044071ed5b9dfd4af8a53728de29c8c6fcb0
Patricio Arluciaga <parluciaga@yahoo.com>
2020-06-16 20:40:22 -0400

    add favicon

--- .history/app/assets/javascripts/application_20200616183827.js
+++ .history/app/assets/javascripts/application_20200616183827.js
@@ -11,12 +11,6 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require jquery
 //= require activestorage
 //= require turbolinks
-//= require semantic-ui
 //= require_tree .
-
-$(document).on('turbolinks:load', function() {
-  $('.ui.dropdown').dropdown();
-})
--- .history/app/assets/javascripts/application_20200616185527.js
+++ .history/app/assets/javascripts/application_20200616185527.js
@@ -16,7 +16,3 @@
 //= require turbolinks
 //= require semantic-ui
 //= require_tree .
-
-$(document).on('turbolinks:load', function() {
-  $('.ui.dropdown').dropdown();
-})
