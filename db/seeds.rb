42a079fe8739305f1b9c6351636e86844306612f
Patricio Arluciaga <parluciaga@yahoo.com>
2020-06-29 14:07:35 -0400

    Finished chat app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Juan", password: "password")
+User.create(username: "John", password: "password")
